0cf32b53cd9da5de41e53fc732fb365ba30c5d32
kayama <shimonk333@gmail.com>
2022-12-15 00:07:32 +0900

    変数とメソッドの名前を変更しました

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -8,22 +8,22 @@ OPTIONS_KEY = params.keys
 OPTIONS_KEY_ORDER = %w[l w c].freeze
 
 def main(params)
-  input = input()
-  file_name = input.keys.map { |x| x == '-' ? ' ' : " #{x}" }
-  file_contents = input.values
+  file_names_contents = input
+  file_names = file_names_contents.keys.map { |i| i == '-' ? ' ' : " #{i}" }
+  file_contents = file_names_contents.values
 
-  lines = number_of_lines(file_contents)
-  words = number_of_words(file_contents)
-  bytes = number_of_bytes(file_contents)
+  number_of_lines = count_lines(file_contents)
+  number_of_words = count_words(file_contents)
+  number_of_bytes = count_bytes(file_contents)
 
-  elements = elements(lines, words, bytes, file_name)
-  elements_with_options = elements_with_options(lines, words, bytes, file_name, params)
+  sizes_names = sort_sizes_names(number_of_lines, number_of_words, number_of_bytes, file_names)
+  sizes_names_with_options = sort_sizes_names_with_options(number_of_lines, number_of_words, number_of_bytes, file_names, params)
 
-  calc_total_elements = calc_total_elements(lines, words, bytes)
-  total_elements = total_elements(calc_total_elements)
-  total_elements_with_options = total_elements_with_options(params, calc_total_elements)
+  sorted_total_sizes = calc_total_sizes(number_of_lines, number_of_words, number_of_bytes)
+  total_sizes = sort_total_sizes(sorted_total_sizes)
+  total_sizes_with_options = sort_total_sizes_with_options(params, sorted_total_sizes)
 
-  output(elements, elements_with_options, total_elements, total_elements_with_options, params)
+  output(sizes_names, sizes_names_with_options, total_sizes, total_sizes_with_options, params)
 end
 
 def input
@@ -34,65 +34,65 @@ def input
   input
 end
 
-def number_of_lines(file_contents)
-  file_contents.map { |x| x.count("\n").to_s.rjust(8) }
+def count_lines(file_contents)
+  file_contents.map { |i| i.count("\n").to_s.rjust(8) }
 end
 
-def number_of_words(file_contents)
-  file_contents.map { |x| x.scan(/[^\s]+/).length.to_s.rjust(8) }
+def count_words(file_contents)
+  file_contents.map { |i| i.scan(/[^\s]+/).length.to_s.rjust(8) }
 end
 
-def number_of_bytes(file_contents)
-  file_contents.map { |x| x.bytesize.to_s.rjust(8) }
+def count_bytes(file_contents)
+  file_contents.map { |i| i.bytesize.to_s.rjust(8) }
 end
 
-def elements(lines, words, bytes, file_name)
-  [lines, words, bytes, file_name].transpose.map(&:join)
+def sort_sizes_names(number_of_lines, number_of_words, number_of_bytes, file_names)
+  [number_of_lines, number_of_words, number_of_bytes, file_names].transpose.map(&:join)
 end
 
-def elements_with_options(lines, words, bytes, file_name, params)
-  elements_with_options = []
-  elements_with_options << lines if params['l']
-  elements_with_options << words if params['w']
-  elements_with_options << bytes if params['c']
-  elements_with_options << file_name
-  elements_with_options.transpose.map(&:join)
+def sort_sizes_names_with_options(number_of_lines, number_of_words, number_of_bytes, file_names, params)
+  sizes_names = []
+  sizes_names << number_of_lines if params['l']
+  sizes_names << number_of_words if params['w']
+  sizes_names << number_of_bytes if params['c']
+  sizes_names << file_names
+  sizes_names.transpose.map(&:join)
 end
 
-def calc_total_elements(lines, words, bytes)
-  calc_total_elements = [lines.map(&:to_i).sum, words.map(&:to_i).sum, bytes.map(&:to_i).sum]
-  sorted_options_key = OPTIONS_KEY.sort_by { |x| OPTIONS_KEY_ORDER.index(x[0]) }
+def calc_total_sizes(number_of_lines, number_of_words, number_of_bytes)
+  calc_total_sizes = [number_of_lines.map(&:to_i).sum, number_of_words.map(&:to_i).sum, number_of_bytes.map(&:to_i).sum]
+  sorted_options_key = OPTIONS_KEY.sort_by { |i| OPTIONS_KEY_ORDER.index(i[0]) }
 
-  [sorted_options_key, calc_total_elements].transpose.to_h
+  [sorted_options_key, calc_total_sizes].transpose.to_h
 end
 
-def total_elements(calc_total_elements)
-  total_lines = calc_total_elements['l'].to_s.rjust(8)
-  total_words = calc_total_elements['w'].to_s.rjust(8)
-  total_bytes = calc_total_elements['c'].to_s.rjust(8)
+def sort_total_sizes(sorted_total_sizes)
+  total_lines = sorted_total_sizes['l'].to_s.rjust(8)
+  total_words = sorted_total_sizes['w'].to_s.rjust(8)
+  total_bytes = sorted_total_sizes['c'].to_s.rjust(8)
 
   "#{total_lines}#{total_words}#{total_bytes} total"
 end
 
-def total_elements_with_options(params, calc_total_elements)
-  total_lines = calc_total_elements['l'].to_s.rjust(8)
-  total_words = calc_total_elements['w'].to_s.rjust(8)
-  total_bytes = calc_total_elements['c'].to_s.rjust(8)
+def sort_total_sizes_with_options(params, sorted_total_sizes)
+  total_lines = sorted_total_sizes['l'].to_s.rjust(8)
+  total_words = sorted_total_sizes['w'].to_s.rjust(8)
+  total_bytes = sorted_total_sizes['c'].to_s.rjust(8)
 
   "#{total_lines if params['l']}#{total_words if params['w']}#{total_bytes if params['c']} total"
 end
 
-def output(elements, elements_with_options, total_elements, total_elements_with_options, params)
+def output(sizes_names, sizes_names_with_options, total_sizes, total_sizes_with_options, params)
   if params.values.any?
-    puts elements_with_options
+    puts sizes_names_with_options
     return unless ARGF.lineno > 1
 
-    puts total_elements_with_options
+    puts total_sizes_with_options
   else
-    puts elements
+    puts sizes_names
     return unless ARGF.lineno > 1
 
-    puts total_elements
+    puts total_sizes
   end
 end
 
