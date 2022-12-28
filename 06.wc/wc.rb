3fa6775fda499dc95fe313c13c1edfd21cb3fe23
kayama <shimonk333@gmail.com>
2022-12-28 18:32:27 +0900

    calc_total_sizesとoutput_countの処理を変更しました

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -4,50 +4,49 @@
 require 'optparse'
 
 def main
-  view_options = option
-  files = parse_input(view_options)
+  view_options = parse_view_options
+  files = parse_input
   total_sizes = calc_total_sizes(files)
-  output(files, total_sizes)
+  output(files, total_sizes, view_options)
 end
 
-def option
+def parse_view_options
   options = ARGV.getopts('lwc')
   { l: options['l'] || options.values.none?,
     w: options['w'] || options.values.none?,
     c: options['c'] || options.values.none? }
 end
 
-def parse_input(view_options)
+def parse_input
   files = []
   while (argf = ARGF.gets(nil))
-    files << { line_count: (argf.count("\n") if view_options[:l]),
-               word_count: (argf.scan(/[^\s]+/).length if view_options[:w]),
-               byte_count: (argf.bytesize if view_options[:c]),
+    files << { line_count: argf.count("\n"),
+               word_count: argf.scan(/[^\s]+/).length,
+               byte_count: argf.bytesize,
                name: ARGF.filename == '-' ? '' : " #{ARGF.filename}" }
   end
   files
 end
 
 def calc_total_sizes(files)
-  { line_count: (files.sum { |v| v[:line_count] } unless files[0][:line_count].nil?),
-    word_count: (files.sum { |v| v[:word_count] } unless files[0][:word_count].nil?),
-    byte_count: (files.sum { |v| v[:byte_count] } unless files[0][:byte_count].nil?),
+  { line_count: files.sum { |v| v[:line_count].to_i },
+    word_count: files.sum { |v| v[:word_count].to_i },
+    byte_count: files.sum { |v| v[:byte_count].to_i },
     name: ' total' }
 end
 
-def output(files, total_sizes)
-  files.each { |v| output_count(v) }
+def output(files, total_sizes, view_options)
+  files.each { |v| output_count(v, view_options) }
   return if files.size == 1
 
-  output_count(total_sizes)
+  output_count(total_sizes, view_options)
 end
 
-def output_count(files)
-  result = [files[:line_count].to_s,
-            files[:word_count].to_s,
-            files[:byte_count].to_s]
-  result.delete('')
-  puts result.map { |v| v.rjust(8) }.join + files[:name]
+def output_count(files, view_options)
+  puts [(files[:line_count].to_s.rjust(8) if view_options[:l]),
+        (files[:word_count].to_s.rjust(8) if view_options[:w]),
+        (files[:byte_count].to_s.rjust(8) if view_options[:c]),
+        files[:name]].join
 end
 
 main
